ac2840632610d28ca4ece5d988602fd87d4e2dbd
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-03-24 17:28:04 -0700

    finish scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,38 +8,29 @@ from bs4 import BeautifulSoup as bs
 
 def scraper():
     # Grabbing the URL page
-    genius_kitchen = r.get(i)
+    social_blade = r.get('https://socialblade.com/instagram/user/selenagomez')
 
     # Grabbing only the html elements of the page
-    gk_html = bs(genius_kitchen.content, 'lxml')
+    sb_html = bs(social_blade.content, 'lxml')
 
-    # Grabbing Real URL and cleaning it
-    real_url = get_real_url(gk_html)
+    results = sb_html.find_all("div", {"class": "stats-top-data-content"})
+    
+    stats = [];
 
-    # Grabbing Name of Dish
-    dish_name = gk_html.find('h1').text.strip(' "\'\t\r\n')
+    for i in results:
+        stats.append(clean_text(i))
 
-    # Stripping html tags and adding ingredients to the list
-    list_of_ingredients = get_ingredients_list(gk_html.find_all('span', class_= 'food'))
-                    
-
-
-# Returns a list of ingredients in the recipe with the html tags stripped
-# Takes in the ingredients with html tags
-def get_ingredients_list(ingredients):
-    list_of_ingredients = []
-    for i in ingredients:
-        list_of_ingredients.append((i.text.strip('\n')).lower())
-    return list_of_ingredients
+    print(stats)    
 
 # Takes in genius kitchens HTML content
 # Finds the real URL, cleans it up, and returns it as a string
-def get_real_url(gk_html):
-    real_url = str(gk_html.find("meta",  property="og:url"))
-    real_url = real_url.replace("<meta content=", "")
-    real_url = real_url.replace("property=\"og:url\"/>", '')
-    real_url = real_url.replace("\"", '')
-    return real_url.strip()
+def clean_text(data):
+    data_to_ret = data.text;
+    data_to_ret = data_to_ret.replace("<div class=\"stats-top-data-content\"", "")
+    data_to_ret = data_to_ret.replace(" style=\"font-size: 0.9em;\">", '')
+    data_to_ret = data_to_ret.replace("</div>", '')
+    data_to_ret = data_to_ret.replace("\"", '')
+    return data_to_ret.strip()
 
 # Calling the scraper method
 scraper()
